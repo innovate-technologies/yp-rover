b56d4ef095eb4bd904161a0bdb9a1228badec723
Maartje Eyskens <meyskens@me.com>
2019-01-07 18:58:36 +0100

    Log fetchstations very verbose

--- internal/tasks/shoutcast/fetchstations.go
+++ internal/tasks/shoutcast/fetchstations.go
@@ -2,24 +2,33 @@ package shoutcast
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/innovate-technologies/yp-rover/internal/tasks"
 	"github.com/innovate-technologies/yp-rover/pkg/shoutcastcom"
+	"github.com/innovate-technologies/yp-rover/pkg/store"
 	"github.com/innovate-technologies/yp-rover/pkg/streamchecker"
 )
 
 // FetchForGenre fetches 50 stations for a genre and dispatches a new job if needed
 func (t *Task) FetchForGenre(genre string, offset int64) ([]tasks.Task, error) {
 	api := shoutcastcom.NewClient(t.config.ShoutcastKey)
+	db, err := store.New(t.config.MySQLURL)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Getting stations for genre %s with offset %d", genre, offset)
 	stations, err := api.GetByGenre(genre, "", "", fmt.Sprintf("%d,50", offset))
 	if err != nil {
 		return nil, err
 	}
 
-	//spew.Dump(stations)
 	for _, station := range stations {
+		log.Printf("Checking station %s", station.ID)
 		urls, _ := api.GeTuneInURLs(station.ID)
+		time.Sleep(300 * time.Millisecond) // for rate limiting
 		if urls == nil || len(urls) == 0 { // non existing station
 			continue
 		}
@@ -32,12 +41,16 @@ func (t *Task) FetchForGenre(genre string, offset int64) ([]tasks.Task, error) {
 		station.ListenURLs = valid
 		time.Sleep(200 * time.Millisecond) // try not to fetch too fast
 		if len(valid) > 0 {
-
+			log.Printf("Saving station %s", station.ID)
+			err := db.AddSHOUTcastStation(station)
+			if err != nil {
+				log.Println(err)
+			}
 		}
-		// TODO: add me to a database
 	}
 
 	if len(stations) != 0 {
+		log.Printf("More stations for genre %s to be fetched!", genre)
 		time.Sleep(5 * time.Second) // try not to fetch too fast
 		return []tasks.Task{
 			tasks.Task{
