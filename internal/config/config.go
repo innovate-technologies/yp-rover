fc374f5bb95a36d27ce3aa922a93c3dba5cf6169
Maartje Eyskens <maartje@eyskens.me>
2019-01-07 15:38:45 +0100

    Set envconfig for ShoutcastKey

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 type Config struct {
-	ShoutcastKey string `required:"true"`
+	ShoutcastKey string `required:"true" envconfig:"shoutcast_key"`
 	RabbitMQURL  string `required:"true" envconfig:"rabbitmq_url"`
 	MySQLURL     string `required:"true" envconfig:"mysql_url"`
 }
