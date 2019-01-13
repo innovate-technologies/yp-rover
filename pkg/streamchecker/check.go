7dbfcf70911647129d65a1679f21cd6e89bb9298
Maartje Eyskens <meyskens@me.com>
2019-01-13 22:39:41 +0100

    Do crazy stuff to support GET for media types

--- pkg/streamchecker/check.go
+++ pkg/streamchecker/check.go
@@ -1,37 +1,38 @@
 package streamchecker
 
 import (
-	"net/http"
+	"context"
 	"strings"
+	"time"
 
 	resty "gopkg.in/resty.v1"
 )
 
 // CheckValidStream will call a stream URL and check if a radio stream is present
-func CheckValidStream(url string) bool {
+func CheckValidStream(streamurl string) bool {
 	resty.SetRedirectPolicy(resty.FlexibleRedirectPolicy(30)) // because radionomy
 	r := resty.R()
 	r.Header.Set("User-Agent", "VLC/3.0.4 LibVLC/3.0.4")
-	resp, err := r.Head(url)
-	if err != nil {
-		return false
+	resp, err := r.Head(streamurl)
+
+	var content string
+	if err == nil { // a version of SHOUTcast returns invalid HTTP on HEAD
+		content = resp.Header().Get("content-type")
 	}
-	content := resp.Header().Get("content-type")
 
-	if resp.StatusCode() == 400 {
-		client := &http.Client{}
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			return false
-		}
-		res, err := client.Do(req)
-		if err != nil {
-			return false
-		}
-
-		content = res.Header.Get("content-type")
-
-		res.Body.Close()
+	if resp.StatusCode() == 400 || content == "" {
+		ctx, cancel := context.WithCancel(context.Background())
+		go func() {
+			time.Sleep(2 * time.Second)
+			cancel()
+		}()
+		r := resty.R()
+		r.Header.Set("User-Agent", "VLC/3.0.4 LibVLC/3.0.4")
+		r.SetContext(ctx)
+		r = r.SetOutput("/dev/null")
+		resp, _ := r.Get(streamurl)
+		content = resp.Header().Get("content-type")
+		resp.RawBody().Close()
 	}
 
 	if strings.Contains(content, "audio/mpeg") || strings.Contains(content, "audio/aacp") || strings.Contains(content, "audio/aac") || strings.Contains(content, "audio/ogg") || strings.Contains(content, "application/ogg") {
