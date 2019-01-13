7f22dd7d052e09e3f2912ac4a3faff60f7c13744
Maartje Eyskens <meyskens@me.com>
2019-01-13 21:24:56 +0100

    Implement fetch for more tunein urls and fetch playlists if needed

--- internal/tasks/tunein/fetchstations.go
+++ internal/tasks/tunein/fetchstations.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/innovate-technologies/yp-rover/internal/tasks"
+	"github.com/innovate-technologies/yp-rover/pkg/playlists"
 	"github.com/innovate-technologies/yp-rover/pkg/store"
 	"github.com/innovate-technologies/yp-rover/pkg/streamchecker"
 	"github.com/innovate-technologies/yp-rover/pkg/tunein"
@@ -16,7 +17,9 @@ func (t *Task) FetchForGenre(genre string, offset int64) ([]tasks.Task, error) {
 	log.Printf("Getting TuneIn stations for genre %s and offset %d\n", genre, offset)
 	api := tunein.NewClient()
 	stations, newOffset, err := api.BrowseStations(genre, offset)
+	log.Println("Getting TuneIn station list")
 	if err != nil {
+		log.Printf("Got error: %s\n", err)
 		return nil, err
 	}
 
@@ -28,10 +31,24 @@ func (t *Task) FetchForGenre(genre string, offset int64) ([]tasks.Task, error) {
 
 	//spew.Dump(stations)
 	for _, station := range stations {
-		log.Printf("Checking %s \n", station.Name)
-		if !streamchecker.CheckValidStream(station.TuneInURL) {
+		log.Printf("Checking %s\n", station.Name)
+		valid := []string{}
+		for _, url := range station.TuneInURLs {
+			if streamchecker.CheckValidStream(url) {
+				valid = append(valid, url)
+			} else if streamchecker.CheckValidPlaylist(url) {
+				entries, err := playlists.GetEntryURLs(url)
+				if err == nil {
+					valid = append(valid, entries...)
+				}
+			}
+		}
+
+		if len(valid) == 0 {
 			continue
 		}
+		station.TuneInURLs = valid
+
 		log.Printf("Saving station %s", station.Name)
 		err := db.AddTuneInStation(station)
 		if err != nil {
